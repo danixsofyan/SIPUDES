9b8519a611e9c11510fa7371f9e913b9edce3de3
danixsofyan <danixsofyan@danixsofyans-MacBook-Pro.local>
2017-03-16 22:58:48 +0700

    database

--- sipudes.sql
+++ sipudes.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Mar 15, 2017 at 10:59 AM
+-- Generation Time: Mar 16, 2017 at 04:58 PM
 -- Server version: 5.5.42
 -- PHP Version: 5.6.10
 
@@ -33,15 +33,16 @@ CREATE TABLE `admin` (
   `status` int(1) DEFAULT NULL,
   `id_penduduk` varchar(50) DEFAULT NULL,
   `akses` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`id_admin`, `username`, `password`, `status`, `id_penduduk`, `akses`) VALUES
-(6, 'dani', '55b7e8b895d047537e672250dd781555', 1, '12345678', 1),
-(7, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1, '12312312', 2);
+(1, 'superadmin', '17c4520f6cfd1ab53d8745e84681eb49', 1, '9879878978', 1),
+(3, 'adm', 'b09c600fddc573f117449b3723f23d64', 1, '879678676', 2),
+(4, 'ahmad', '61243c7b9a4022cb3f8dc3106767ed12', 1, '897980798', 1);
 
 -- --------------------------------------------------------
 
@@ -50,23 +51,22 @@ INSERT INTO `admin` (`id_admin`, `username`, `password`, `status`, `id_penduduk`
 --
 
 CREATE TABLE `agama` (
-  `id_agama` varchar(10) NOT NULL,
+  `id_agama` int(10) NOT NULL,
   `agama` varchar(30) DEFAULT NULL,
   `status` int(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `agama`
 --
 
 INSERT INTO `agama` (`id_agama`, `agama`, `status`) VALUES
-('000000001', 'Islam', 1),
-('000000002', 'Kristen', 1),
-('000000003', 'Katholik', 1),
-('000000004', 'Hindu', 1),
-('000000005', 'Budha', 1),
-('000000006', 'Khonghucu', 1),
-('000000007', 'a', 1);
+(1, 'Islam', 1),
+(2, 'Kristen', 1),
+(3, 'Katholik', 1),
+(4, 'Hindu', 1),
+(5, 'Budha', 1),
+(6, 'Khonghucu', 1);
 
 -- --------------------------------------------------------
 
@@ -75,18 +75,19 @@ INSERT INTO `agama` (`id_agama`, `agama`, `status`) VALUES
 --
 
 CREATE TABLE `desa` (
-  `desa` varchar(50) NOT NULL,
+  `id_desa` int(11) unsigned NOT NULL,
+  `desa` varchar(50) DEFAULT NULL,
   `kecamatan` varchar(50) NOT NULL,
   `kabupaten` varchar(50) NOT NULL,
   `kepala_desa` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `desa`
 --
 
-INSERT INTO `desa` (`desa`, `kecamatan`, `kabupaten`, `kepala_desa`) VALUES
-('CIUMBULEUIT', 'CIDADAP', 'KOTA BANDUNG', 'KEPALA DESA');
+INSERT INTO `desa` (`id_desa`, `desa`, `kecamatan`, `kabupaten`, `kepala_desa`) VALUES
+(1, 'Ciumbuleuit', 'Cidadap', 'Bandung', 'Aceng Denok ST');
 
 -- --------------------------------------------------------
 
@@ -95,7 +96,7 @@ INSERT INTO `desa` (`desa`, `kecamatan`, `kabupaten`, `kepala_desa`) VALUES
 --
 
 CREATE TABLE `file` (
-  `id_kategori` varchar(30) DEFAULT NULL,
+  `id_kategori` int(30) DEFAULT NULL,
   `nik` varchar(30) DEFAULT NULL,
   `file` text,
   `status` int(1) DEFAULT NULL
@@ -106,7 +107,7 @@ CREATE TABLE `file` (
 --
 
 INSERT INTO `file` (`id_kategori`, `nik`, `file`, `status`) VALUES
-('000000001', '9879878978', 'update201608280143400000000019879878978.pdf', 1);
+(1, '9879878978', 'update201608280143400000000019879878978.pdf', 1);
 
 -- --------------------------------------------------------
 
@@ -115,20 +116,20 @@ INSERT INTO `file` (`id_kategori`, `nik`, `file`, `status`) VALUES
 --
 
 CREATE TABLE `kategori` (
-  `id_kategori` varchar(30) NOT NULL,
+  `id_kategori` int(30) NOT NULL,
   `kategori` varchar(50) DEFAULT NULL,
   `status` int(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `kategori`
 --
 
 INSERT INTO `kategori` (`id_kategori`, `kategori`, `status`) VALUES
-('000000001', 'KTP', 1),
-('000000002', 'Akte', 1),
-('000000003', 'Surat Nikah', 1),
-('000000004', 'Dokumen Penting Lainnya', 1);
+(1, 'KTP', 1),
+(2, 'Akte', 1),
+(3, 'Surat Nikah', 1),
+(4, 'Dokumen Penting Lainnya', 1);
 
 -- --------------------------------------------------------
 
@@ -137,8 +138,8 @@ INSERT INTO `kategori` (`id_kategori`, `kategori`, `status`) VALUES
 --
 
 CREATE TABLE `kategori_klasifikasi` (
-  `id_kategori` varchar(20) DEFAULT NULL,
-  `id_klasifikasi` varchar(10) DEFAULT NULL,
+  `id_kategori` int(20) DEFAULT NULL,
+  `id_klasifikasi` int(10) DEFAULT NULL,
   `status` int(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -147,14 +148,14 @@ CREATE TABLE `kategori_klasifikasi` (
 --
 
 INSERT INTO `kategori_klasifikasi` (`id_kategori`, `id_klasifikasi`, `status`) VALUES
-('000000001', '000000002', 1),
-('000000004', '000000002', 1),
-('000000002', '000000003', 1),
-('000000003', '000000002', 1),
-('000000002', '000000002', 1),
-('000000003', '000000003', 1),
-('000000004', '000000003', 1),
-('000000001', '000000003', 1);
+(1, 2, 1),
+(4, 2, 1),
+(2, 3, 1),
+(3, 2, 1),
+(2, 2, 1),
+(3, 3, 1),
+(4, 3, 1),
+(1, 3, 1);
 
 -- --------------------------------------------------------
 
@@ -163,26 +164,24 @@ INSERT INTO `kategori_klasifikasi` (`id_kategori`, `id_klasifikasi`, `status`) V
 --
 
 CREATE TABLE `kk` (
-  `id_kk` varchar(20) NOT NULL,
+  `id_kk` int(20) NOT NULL,
   `no_kk` varchar(50) DEFAULT NULL,
   `rt` varchar(5) DEFAULT NULL,
   `rw` varchar(5) DEFAULT NULL,
   `kk` varchar(30) DEFAULT NULL,
   `status` varchar(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `kk`
 --
 
 INSERT INTO `kk` (`id_kk`, `no_kk`, `rt`, `rw`, `kk`, `status`) VALUES
-('000000001', '76986987687687', '01', '02', '9879878978', '1'),
-('000000002', '786978698768768', '09', '09', '879678676', '1'),
-('000000003', '8907987897', '09', '08', '769878767', '1'),
-('000000004', '89798787', '08', '09', '897980798', '1'),
-('000000005', '7698687967', '07', '08', '67575577667', '1'),
-('000000006', '2', '1', '1', '12312312', '1'),
-('000000007', '1234567', '6', '11', '', '1');
+(1, '76986987687687', '01', '02', '9879878978', '1'),
+(2, '786978698768768', '09', '09', '879678676', '1'),
+(3, '8907987897', '09', '08', '769878767', '1'),
+(4, '89798787', '08', '09', '897980798', '1'),
+(5, '7698687967', '07', '08', '67575577667', '1');
 
 -- --------------------------------------------------------
 
@@ -191,20 +190,20 @@ INSERT INTO `kk` (`id_kk`, `no_kk`, `rt`, `rw`, `kk`, `status`) VALUES
 --
 
 CREATE TABLE `klasifikasi` (
-  `id_klasifikasi` varchar(10) DEFAULT NULL,
+  `id_klasifikasi` int(10) unsigned NOT NULL,
   `klasifikasi` varchar(50) DEFAULT NULL,
   `status` int(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `klasifikasi`
 --
 
 INSERT INTO `klasifikasi` (`id_klasifikasi`, `klasifikasi`, `status`) VALUES
-('000000001', 'Anak-anak', 1),
-('000000002', 'Dewasa', 1),
-('000000003', 'Orang Tua', 1),
-('000000004', 'Remaja', 1);
+(1, 'Anak-anak', 1),
+(2, 'Dewasa', 1),
+(3, 'Orang Tua', 1),
+(4, 'Remaja', 1);
 
 -- --------------------------------------------------------
 
@@ -214,7 +213,7 @@ INSERT INTO `klasifikasi` (`id_klasifikasi`, `klasifikasi`, `status`) VALUES
 
 CREATE TABLE `klasifikasi_penduduk` (
   `nik` varchar(50) DEFAULT NULL,
-  `id_klasifikasi` varchar(30) DEFAULT NULL
+  `id_klasifikasi` int(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -222,14 +221,12 @@ CREATE TABLE `klasifikasi_penduduk` (
 --
 
 INSERT INTO `klasifikasi_penduduk` (`nik`, `id_klasifikasi`) VALUES
-('9879878978', '000000002'),
-('879678676', '000000002'),
-('76798697786767', '000000002'),
-('769878767', '000000002'),
-('897980798', '000000003'),
-('67575577667', '000000003'),
-('12312312', '000000003'),
-('12345678', '000000002');
+('9879878978', 2),
+('879678676', 2),
+('76798697786767', 2),
+('769878767', 2),
+('897980798', 3),
+('67575577667', 3);
 
 -- --------------------------------------------------------
 
@@ -258,14 +255,12 @@ CREATE TABLE `penduduk` (
 --
 
 INSERT INTO `penduduk` (`nik`, `nama`, `tempat_lahir`, `tanggal_lahir`, `jk`, `golongan_darah`, `alamat`, `pekerjaan`, `kewarganegaraan`, `id_agama`, `id_kk`, `foto`, `status`) VALUES
-('12312312', 'ASDAS', 'ASD', '30/11/1991', 'Laki-laki', 'A', 'JJ', 'JJ', 'INDONESIA', '000000005', '000000006', '', 1),
-('12345678', 'DANI SOFYAN', 'BANDUNG', '14/11/1993', 'Laki-laki', 'A', 'CIUMBULEUIT', 'MAHASISWA', 'INDONESIA', '000000001', '000000007', '', 1),
 ('67575577667', 'SURYA DINATA', 'TUBAN', '12/09/1998', 'Laki-laki', 'AB', 'DSN ALASTUWO ', 'PNS', 'INDONESIA', '000000001', '000000005', 'uploadfoto2016083006161467575577667.jpeg', 1),
 ('76798697786767', 'CAMELIA', 'TUBAN', '12/09/1998', 'Perempuan', 'A', 'JALAN AHMAD YANI', 'IBU RUMAH TANGGA', 'INDONESIA', '000000001', '000000002', 'uploadfoto2016083006100676798697786767.jpeg', 1),
 ('769878767', 'FANA MAYA', 'BOJONEGORO', '12/08/1997', 'Laki-laki', 'AB', 'JALAN SUROYO DINO', 'PNS', 'INDONESIA', '000000001', '000000003', 'uploadfoto20160830061254769878767.jpeg', 1),
 ('879678676', 'ASAL COBA', 'TEMPAT LAHIR', '01/08/2016', 'Laki-laki', 'A', 'JALAN JALAN KE KOTA', 'PETANI', 'INDONESIA', '000000001', '000000002', 'uploadfoto20160829182418879678676.jpeg', 1),
 ('897980798', 'MUHAMMAD ROIISUL ABIDIN', 'TUBAN', '12/07/1998', 'Laki-laki', 'B', 'JALAN AHMAD YANI', '-', 'INDONESIA', '000000001', '000000004', 'uploadfoto20160830061438897980798.jpeg', 1),
-('9879878978', 'TEJA PAKU ALAM', 'TUBAN', '01/08/1998', 'Laki-laki', 'A', 'JALAN BUNTU', 'PETANI', 'INDONESIA', '000000001', '000000001', 'uploadfoto201608280140499879878978.jpeg', 2);
+('9879878978', 'TEJA PAKU ALAM', 'TUBAN', '01/08/1998', 'Laki-laki', 'A', 'JALAN BUNTU', 'PETANI', 'INDONESIA', '000000001', '000000001', 'uploadfoto201608280140499879878978.jpeg', 1);
 
 --
 -- Indexes for dumped tables
@@ -287,7 +282,7 @@ ALTER TABLE `agama`
 -- Indexes for table `desa`
 --
 ALTER TABLE `desa`
-  ADD PRIMARY KEY (`desa`);
+  ADD PRIMARY KEY (`id_desa`);
 
 --
 -- Indexes for table `kategori`
@@ -300,6 +295,12 @@ ALTER TABLE `kategori`
 --
 ALTER TABLE `kk`
   ADD PRIMARY KEY (`id_kk`);
+
+--
+-- Indexes for table `klasifikasi`
+--
+ALTER TABLE `klasifikasi`
+  ADD PRIMARY KEY (`id_klasifikasi`);
 
 --
 -- Indexes for table `penduduk`
@@ -315,7 +316,32 @@ ALTER TABLE `penduduk`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `agama`
+--
+ALTER TABLE `agama`
+  MODIFY `id_agama` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `desa`
+--
+ALTER TABLE `desa`
+  MODIFY `id_desa` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(30) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `kk`
+--
+ALTER TABLE `kk`
+  MODIFY `id_kk` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `klasifikasi`
+--
+ALTER TABLE `klasifikasi`
+  MODIFY `id_klasifikasi` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
